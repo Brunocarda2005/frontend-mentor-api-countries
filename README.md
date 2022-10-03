5c3456eee7fc893996fbd93b1866a65d5ee6c54e
bruno <97346838+Brunocarda2005@users.noreply.github.com>
2022-10-03 19:11:24 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# frontend-mentor-api-countries-
-this is a project where i can improve my skills consuming apis 
+- Ver todos los países desde la API en la página de inicio
+- Buscar un país mediante un campo de "entrada".
+- Filtrar los países por región
+- Haga clic en un país para ver información más detallada en una página aparte
+- Haga clic en los países fronterizos en la página de detalles
+- Alternar el esquema de colores entre el modo claro y el oscuro *(opcional)*
+Traducido con DeepL https://www.deepl.com/app/?utm_source=android&utm_medium=app&utm_campaign=share-translation
---
+++ main.js
@@ -0,0 +1,156 @@
+// ===================== dark Ligth ============================
+
+//vars 
+
+const BTNdarkLight = document.querySelector('.app__header__top__dark-light');
+const app = document.querySelector('body');
+
+BTNdarkLight.addEventListener('click' , () => {
+  
+  app.classList.toggle('dark');
+  
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ==================== menu filter =======================
+
+// vars
+
+const filterBTN = document.querySelector('.app__nav__filter');
+const filter = document.querySelector('.app__nav__filter__region');
+const filterRegion = document.querySelectorAll('#list');
+
+// filter show
+
+filterBTN.addEventListener('click' , () => {
+  
+  
+  filter.classList.toggle('show');
+  
+  
+})
+
+
+filterRegion.forEach( (e) => {
+  
+  
+  e.addEventListener('click' , () => {
+    
+    generatorCard(`https://restcountries.com/v3.1/region/${e.innerHTML}`);
+    
+  })
+  
+  
+})
+
+
+
+
+
+
+// =================== consumo de api ===========================
+
+
+// vars 
+
+const windowCard = document.querySelector('.app__main__cards');
+
+
+// card generator
+
+const generatorCard = async (url) => {
+  
+  windowCard.innerHTML = '';
+  
+   
+  
+  
+  await fetch(url)
+    .then((element) => element.json() )
+    .then((data) => {
+        
+        
+        data.forEach((e) => {
+          
+           windowCard.innerHTML += `
+             <div class="app__main__cards__card">
+               <img src="${e.flags.png}" alt="">
+               <div class="app__main__cards__card__details">
+                 <div class="app__main__cards__card__details__d">
+                 <h5 class="app__main__cards__card__details__title">${e.name.common}</h5>
+                   <div class="app__main__cards__card__details__list">
+                     <p><b>Population :</b>${e.population}</p>
+                     <p><b>Region :</b>${e.region}</p>
+                     <p><b>Capital :</b>${e.capital[0]}</p>
+                   </div>
+                 </div>
+               </div>
+             </div>`
+          
+          
+        
+          
+          
+        })
+    })
+
+}
+
+generatorCard('https://restcountries.com/v3.1/all');
+
+
+
+
+
+
+
+
+
+
+// ============================ search =================================
+
+
+
+const searchInput = document.getElementById('search');
+const autoComplete = document.querySelector('.show__auto_complete');
+const titleComplete = document.querySelector('.show__auto_complete__myText');
+let value = '';
+
+
+
+searchInput.addEventListener('keyup' , () => {
+  
+  value = searchInput.value;
+  titleComplete.innerHTML = value;
+  
+  if (value == '') {
+    
+    autoComplete.style = 'display: none;'
+    
+  }else {
+    
+    autoComplete.style = 'display: flex;'
+    
+  }
+  
+})
+
+
+searchInput.addEventListener('change' , () => {
+  
+  generatorCard(`https://restcountries.com/v3.1/name/${value}`);
+  
+})
